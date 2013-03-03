88a1ef5a7c559f2cb2de352813ff11e057399dac
r0k3t <knagorski@gmail.com>
2013-03-03 15:25:14 -0500

    Howdy!

---
+++ WaitScreenDemo/ViewModel/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WaitScreenDemo.Annotations;
+
+namespace WaitScreenDemo.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) 
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WaitScreenDemo/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Input;
+using WaitScreenDemo.Annotations;
+
+namespace WaitScreenDemo.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private RelayCommand _updateCommand;
+        private bool _loadComplete;
+        private BackgroundWorker bg;
+
+        public MainWindowViewModel()
+        {
+            bg = new BackgroundWorker();
+            bg.DoWork += (s, e) => Thread.Sleep(5000);
+            bg.RunWorkerCompleted += (s, e) => LoadComplete = true;
+            bg.RunWorkerAsync();
+        }
+
+        public bool LoadComplete
+        {
+            get { return !_loadComplete; }
+            set
+            {
+                _loadComplete = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand UpdateCommand
+        {
+            get { return _updateCommand ?? (_updateCommand = new RelayCommand(x => LoadComplete = true)); }
+        }
+       
+    }
+}
